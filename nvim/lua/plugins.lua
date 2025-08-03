6e3c2ae193317e18c41257f97dc45b74c177e03c
Rasmus Nygren <rasmus.nygren99@gmail.com>
2025-08-03 21:40:54 +0200

    move nvim plugins to lazy.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,80 +1,73 @@
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-        vim.cmd [[packadd packer.nvim]]
-        return true
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
     end
-    return false
 end
+vim.opt.rtp:prepend(lazypath)
 
-
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function()
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-
-    --  use 'morhetz/gruvbox'
-    use 'ellisonleao/gruvbox.nvim'
+require("lazy").setup({
+    -- Colorscheme
+    "ellisonleao/gruvbox.nvim",
 
     -- LSP
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+    "neovim/nvim-lspconfig",
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+    -- Autocompletion
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
 
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
-        }
-    }
-
-
-    -- -- UHM
-    -- use 'jose-elias-alvarez/null-ls.nvim'
-    -- use 'jay-babu/mason-null-ls.nvim'
-
+    -- Snippets
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
 
     -- Clojure
-    use 'Olical/conjure'
-    use 'guns/vim-sexp'
-    use 'tpope/vim-sexp-mappings-for-regular-people'
+    "Olical/conjure",
+    "guns/vim-sexp",
+    "tpope/vim-sexp-mappings-for-regular-people",
 
-    -- Essentials
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+    "nvim-treesitter/nvim-treesitter-textobjects",
 
+    -- Telescope
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+    },
 
-    -- tpope
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-surround'
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-repeat'
+    -- tpope plugins
+    "tpope/vim-commentary",
+    "tpope/vim-surround",
+    "tpope/vim-fugitive",
+    "tpope/vim-repeat",
 
-    use({
-    "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
+    -- Markdown preview
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
 })
-
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
